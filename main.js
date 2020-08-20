83aee7eb92e1ca795c1ad194f2e0357823074fc8
GreenpeaceKT <68542998+GreenpeaceKT@users.noreply.github.com>
2020-08-20 23:47:47 +0900

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,6 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-  client.user.setPresence({ game: { name: 'MAZE-SYSTEM' } });
   console.log(`Logged in as ${client.user.tag}!`);
 });
 

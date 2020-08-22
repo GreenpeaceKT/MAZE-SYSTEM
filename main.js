621ff869d731d2af894d4d8f06c2b68d89c11b8f
GreenpeaceKT <68542998+GreenpeaceKT@users.noreply.github.com>
2020-08-22 09:49:23 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,22 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const config = require('./config.json')
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
+client.on('message', message =>{
+
+if(message.content.indexOf(config.prefix) !== 0) return;
+    const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+    const command = args.shift().toLowerCase();
+
+if(command === "say"){
+        const say_message = args.join(" ");
+        message.delete().catch(msg=>{})
+        message.channel.send(say_message);
+    }
 
 
 client.login(process.env.DISCORD_BOT_TOKEN);

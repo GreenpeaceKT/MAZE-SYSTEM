f481f2bd31d15deea43dd43dcecceaf787cbb678
GreenpeaceKT <68542998+GreenpeaceKT@users.noreply.github.com>
2020-08-30 15:14:33 +0900

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 const config = require('./config.json')
 
 client.on('ready', () => {
-  message.channels.get('749265842125930577')send.('Botが起動しました');
+  message.channels.get('749265842125930577').send('Botが起動しました');
   console.log(`Logged in as ${client.user.tag}!`);
 });
 

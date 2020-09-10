2a531b3428c8eabe12f7b75fbeeb9ab43031d947
GreenpeaceKT <68542998+GreenpeaceKT@users.noreply.github.com>
2020-09-11 00:18:14 +0900

    Update main.js

--- main.js
+++ main.js
@@ -3,8 +3,7 @@ const config = require('./config.json');
 const client = new Discord.Client();
 const MAZE = '731348290267906068'
 const MEMBERCOUNT = '752067627018485801'
-const invite = await message.channel.createInvite()
-message.channel.send(invite.url)
+
 
 client.on('ready', () => { 
   console.log(`Logged in as ${client.user.tag}!`);

ab7d887da3864ed7a53578f4fb93d94bb81a1bfc
GreenpeaceKT <68542998+GreenpeaceKT@users.noreply.github.com>
2020-09-21 09:52:26 +0900

    Update main.js

--- main.js
+++ main.js
@@ -53,19 +53,18 @@ if(command === "addrole"){
     }
 
 if(command === "ban"){
-        if (!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send("Invalid Permissions")
-        let User = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0])
-        if (!User) return message.channel.send("Invalid User")
-        if (User.hasPermission("BAN_MEMBERS")) return message.reply("Invalid Permissions")
+        if (!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send("Invalid Permissions");
+        let User = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
+        if (!User) return message.channel.send("Invalid User");
+        if (User.hasPermission("BAN_MEMBERS")) return message.reply("Invalid Permissions");
         let banReason = args.join(" ").slice(22);
         if (!banReason) {
           banReason = "None"
         }
 
-        User.ban({reason: banReason})
-   
-  
-}
+        User.ban({reason: banReason});
+
+})
 
 
 client.login(process.env.DISCORD_BOT_TOKEN);

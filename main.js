31ff31ccaff3d4f31f2ff73f48363c54303f73f4
GreenpeaceKT <68542998+GreenpeaceKT@users.noreply.github.com>
2020-10-11 08:27:55 +0900

    Update main.js

--- main.js
+++ main.js
@@ -51,25 +51,7 @@ if(command === "addrole"){
     }
 
 if(command === "ban"){
-        const args = message.content.split(' ').slice(1);
-
-const user = message.mentions.users.first();
-const banReason = args.slice(1).join(' ');
-if (!user) {
-  try {
-    if (!message.guild.members.get(args.slice(0, 1).join(' '))) throw new Error('Couldn\' get a Discord user with this userID!');
-    user = message.guild.members.get(args.slice(0, 1).join(' '));
-    user = user.user;
-  } catch (error) {
-    return message.reply('Could not get a Discord user with this userID!');
-  }
-}
-if (user === message.author) return message.channel.send('You can not ban yourself');
-if (!reason) return message.reply('You forgot to enter a reason for this ban!');
-if (!message.guild.member(user).bannable) return message.reply('You can not ban this user because you the bot has not sufficient permissions!');
-
-
-}
+        
 
 });
 

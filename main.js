908af02bb1c2a973b6b31162e4f33eb7cb71558d
GreenpeaceKT <68542998+GreenpeaceKT@users.noreply.github.com>
2020-12-26 15:30:12 +0900

    Update main.js

--- main.js
+++ main.js
@@ -55,7 +55,7 @@ if(command === "addrole" && !message.member.roles.cache.has('742228974469382236'
 if(command === "rand"){
         const randoma = Math.floor( Math.random() * 5 )+ 1;
         const randomb = Math.floor( Math.random() * 8 )+ 1;
-        message.chanel.send(randoma","randomb);
+        message.chanel.send(randoma randomb);
     }
 
 
